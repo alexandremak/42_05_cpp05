61ca41caf2b28821a30e59bc237eca4dec91ab81
Alexandre Mak <amak@Alexandres-Air.home>
2024-06-19 02:10:25 +0100

    ex00: finished Bureaucrat Class header and cpp files

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,7 +6,77 @@
 /*   By: amak <amak@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 21:51:10 by amak              #+#    #+#             */
-/*   Updated: 2024/06/11 21:51:11 by amak             ###   ########.fr       */
+/*   Updated: 2024/06/19 02:08:08 by amak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name("Unnamed"), _grade(150) {
+	std::cout << BLUE << "[BUREAUCRAT]: default constructor called" << RESET
+		<< std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &otherBureaucrat) : 
+	_name(otherBureaucrat._name),
+	_grade(otherBureaucrat._grade) {
+	std::cout << BLUE << "[BUREAUCRAT]: copy constructor called" << RESET
+		<< std::endl;
+}
+
+Bureaucrat	&Bureaucrat::operator=(const Bureaucrat &otherBureaucrat) {
+	std::cout << BLUE << "[BUREAUCRAT]: copy assignement constructor called"
+		<< RESET << std::endl;
+	if (this != &otherBureaucrat)
+		this->_grade = otherBureaucrat._grade;
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat() {
+	std::cout << BLUE << "[BUREAUCRAT]: destructor called" << RESET
+		<< std::endl;
+}
+
+Bureaucrat::Bureaucrat(std::string name, unsigned int grade) : 
+	_name(name), _grade(grade) {
+	std::cout << BLUE << "[BUREAUCRAT]: parameters constructor called" << RESET
+		<< std::endl;	
+	if (grade < 1)
+		throw GradeTooLowException();
+	else if (grade > 150)
+		throw GradeTooHighException();
+}
+
+std::string	Bureaucrat::getName() const {
+	return(this->_name);
+}
+
+unsigned int	Bureaucrat::getGrade() const {
+	return(this->_grade);
+}
+
+void	Bureaucrat::incrementGrade() {
+	if (this->_grade + 1 > 150)
+		throw GradeTooHighException();
+	this->_grade += 1;
+}
+
+void	Bureaucrat::decrememtGrade() {
+	if (this->_grade - 1 < 1)
+		throw GradeTooHighException();
+	this->_grade -= 1;
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw() {
+	return ("Grade value is higher than 150");
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw() {
+	return ("Grade value is lower than 1");
+}
+
+std::ostream	&operator<<(std::ostream &output, const Bureaucrat &bureaucrat)
+{
+	output << bureaucrat.getName() << "bureaucrat grade "
+		<< bureaucrat.getGrade() << "." << std::endl;
+}
--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: amak <amak@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 21:50:56 by amak              #+#    #+#             */
-/*   Updated: 2024/06/11 22:01:24 by amak             ###   ########.fr       */
+/*   Updated: 2024/06/19 02:06:32 by amak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 
 # include <iostream>
 # include <string>
+# include <stdexcept>
+
+const std::string BLUE = "\033[34m";
+const std::string RESET = "\033[0m";
 
 class Bureaucrat {
 	public:
@@ -22,16 +26,28 @@ class Bureaucrat {
 		Bureaucrat(const Bureaucrat &otherBureaucrat);
 		Bureaucrat	&operator=(const Bureaucrat &otherBureaucrat);
 		~Bureaucrat();
-		std::string	getName();
-		std::string	getGrade();
-		void		GradeTooHighException();
-		void		GradeTooLowException();
+
+		Bureaucrat(std::string name, unsigned int grade);
+		std::string		getName() const;
+		unsigned int	getGrade() const;
+		void			incrementGrade();
+		void			decrememtGrade();
+		
+		class GradeTooHighException : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
+		
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char *what() const throw();
+		};
 		
 	private:
 		const std::string	_name;
 		unsigned int		_grade;	
 };
 
-std::ostream	&operator<<(std::ostream &output, const Bureaucrat &otherBureaucrat);
+std::ostream	&operator<<(std::ostream &output, const Bureaucrat &bureaucrat);
 
 #endif
