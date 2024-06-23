b361fef3f917a84383d437c1f9c4f5b0c5804f85
Alexandre Mak <amak@Alexandres-Air.home>
2024-06-23 14:14:11 +0100

    ex01: remove extra tabs main.cpp

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amak <amak@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 21:51:20 by amak              #+#    #+#             */
-/*   Updated: 2024/06/22 22:57:44 by amak             ###   ########.fr       */
+/*   Updated: 2024/06/23 14:12:45 by amak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,16 @@ int main() {
 	// unsigned int grade = 1;
 	unsigned int grade = 150;
 
-
 	try {
 		Bureaucrat montenegro("Montenegro", grade);
 		std::cout << montenegro << std::endl;
-		
+
 		montenegro.incrementGrade();
 		std::cout << montenegro << std::endl;
 
 		montenegro.decrementGrade();
 		std::cout << montenegro << std::endl;
-		
+
 		Form formulario("Autorizacao", 150, 150);
 		std::cout << formulario << std::endl;
 
